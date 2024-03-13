cc7cfee832e861cc68e4c8b8c6fbbf54d96d1956
ilia-kmrv <i.v.kmrv@yandex.ru>
2024-03-13 11:27:04 +0300

    feat: check user email before post create

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.catsgram.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
@@ -9,13 +11,23 @@ import java.util.List;
 @Service
 public class PostService {
     private final List<Post> posts = new ArrayList<>();
+    private final UserService userService;
+
+    @Autowired
+    public PostService(UserService userService){
+        this.userService = userService;
+    }
 
     public List<Post> findAll() {
         return posts;
     }
 
     public Post create(Post post) {
-        posts.add(post);
-        return post;
+        if (userService.findUserByEmail(post.getAuthor()).isEmpty()){
+            throw new UserNotFoundException("Пользователь " + post.getAuthor() + " не найден");
+        } else {
+            posts.add(post);
+            return post;
+        }
     }
 }
